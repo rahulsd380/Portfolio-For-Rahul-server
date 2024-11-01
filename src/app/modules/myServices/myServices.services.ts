7f7656c572c2da32d4739068e53ab7402936a8ca
Rahul Sutradhar <rahulsd380@gmail.com>
2024-11-01 08:41:08 +0600

    Project setup

---
+++ src/app/modules/myServices/myServices.services.ts
@@ -0,0 +1,54 @@
+import { TBike } from "./myServices.interface";
+import { Bike } from "./myServices.model";
+
+const createBike = async (payload: TBike) => {
+
+  const { name, image, description, pricePerHour, cc, year, model, brand } = payload;
+
+  const payloadData = {
+    name : name || "",
+    image : image || "",
+    description : description || "",
+    pricePerHour : pricePerHour || "",
+    isAvailable : true,
+    cc : cc || "",
+    year : year || "",
+    model : model || "",
+    brand : brand || "",
+  }
+
+  const result = await Bike.create(payloadData);
+  return result;
+};
+
+
+const getAllBikes = async () => {
+  const result = await Bike.find();
+  return result;
+};
+
+const getSingleBikeById = async (bikeId: string) => {
+  const result = await Bike.findById(bikeId);
+  return result;
+};
+
+const updateBike = async (id : string, payload : Partial<TBike>) => {
+  const result = await Bike.findByIdAndUpdate(id, payload, {
+    new: true,
+    runValidators: true,
+  });
+  return result;
+};
+
+const deleteBike = async (id: string) => {
+  const result = await Bike.findByIdAndDelete(id);
+  return result;
+};
+
+export const BikeServices = {
+  createBike,
+  getAllBikes,
+  updateBike,
+  deleteBike,
+  getSingleBikeById,
+};
