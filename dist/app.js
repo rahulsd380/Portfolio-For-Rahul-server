6bc9937a5fad1eb1a7e7f6d0a790e7e271fe6360
Rahul Sutradhar <rahulsd380@gmail.com>
2025-04-01 19:51:58 +0600

    Added cors

--- dist/app.js
+++ dist/app.js
@@ -30,7 +30,7 @@ app.use((0, cors_1.default)({
         'https://portfolio-rahul-admin.vercel.app',
         'https://developer-rahul.vercel.app',
         'http://localhost:5173',
-        'http://localhost:5174',
+        'http://localhost:3000',
     ],
     credentials: true,
 }));
